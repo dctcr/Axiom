58cf9c928d8c718979e450b46dc1ac9fb7116e7c
Angel <ajex.murg@gmail.com>
2026-01-14 13:42:42 -0700

    + Coin Flip command

---
+++ src/commands/general/coinflip.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("coinflip")
+    .setDescription("Flip a coin!"),
+
+  /** @param {import("discord.js").ChatInputCommandInteraction} interaction */
+  async execute(interaction) {
+    const results = ["Heads!", "Tails!"];
+    const result = results[Math.floor(Math.random() * results.length)];
+
+    return interaction.reply({ content: result });
+  },
+};
