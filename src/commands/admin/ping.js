8460eb5ba09976835a1a62ab9f9158834a3c772b
Angel <ajex.murg@gmail.com>
2026-01-16 15:30:58 -0700

    + Latency Command

--- src/commands/admin/ping.js
+++ src/commands/admin/ping.js
@@ -1,15 +1,37 @@
-const { SlashCommandBuilder, MessageFlags } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  MessageFlags,
+  ContainerBuilder,
+  PermissionFlagsBits,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with Pong!"),
+    .setDescription("Check client latency!")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
   /** @param {import("discord.js").ChatInputCommandInteraction} interaction */
   async execute(interaction) {
+    // Guild Check
+    if (!interaction.inGuild())
+      return interaction.reply({
+        content: `This command can only be used in a guild!`,
+        flags: MessageFlags.Ephemeral,
+      });
+
+    // Create Container - Send Response
+    const latency = interaction.client.ws.ping;
+    const container = new ContainerBuilder()
+      .addTextDisplayComponents((text) => text.setContent(`## Client Latency`))
+      .addSeparatorComponents((s) => s)
+      .addTextDisplayComponents((text) =>
+        text.setContent(`Current Ping: ${latency}ms`)
+      );
+
     return interaction.reply({
-      content: "Ping",
-      flags: MessageFlags.Ephemeral
+      components: [container],
+      flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
     });
-  }
+  },
 };
